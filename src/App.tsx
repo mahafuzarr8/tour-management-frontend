4c8209559252cc700614b65246d51589514be22d
Radiat09 <rzsianboyridoy10@gmail.com>
2025-08-11 23:51:11 +0600

    initialize project with bun,react,typescript,tailwind css,shadcn

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>this is a simple react app with shadcn</h1>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
